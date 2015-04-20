2295ca6b6c8436f46c634f0e47f957ba2fb2cb3c
Gabe Williamson <gabe@stonefin.com>
2015-04-20 13:50:23 -0500

    First Commit! Sweet trello archiver.

---
+++ Archiver.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quartz;
+using TrelloNet;
+using Common.Logging;
+
+namespace TrelloArchiver
+{
+  class ArchiveSetting
+  {
+    public string ListID { get; set; }
+    public TimeSpan Aging { get { return TimeSpan.FromDays(double.Parse(System.Configuration.ConfigurationManager.AppSettings["daysToArchive"] ?? "14")); } }
+  }
+  public class ArchiverJob : IJob
+  {
+    private string _key;
+    private string _secret;
+    private string _authToken;
+    private ITrello _trello;
+    private ILog _log = LogManager.GetCurrentClassLogger();
+    public ArchiverJob() : this(null,null,null)
+    {
+      
+    }
+
+    public ArchiverJob(ITrello trello = null, string key = null, string secret = null)
+    {
+      this._key = key ?? System.Configuration.ConfigurationManager.AppSettings["trellokey"];
+      this._secret = secret ?? System.Configuration.ConfigurationManager.AppSettings["trellosecret"];
+      this._authToken = System.Configuration.ConfigurationManager.AppSettings["trelloauthtoken"];
+      this._trello = trello ?? new Trello(this._key);
+      if(_authToken == null) {
+        string msg = "You must authorize this app for your trello account. Use the url: " + _trello.GetAuthorizationUrl("Trello Archiver",Scope.ReadWrite,Expiration.Never).ToString();
+        _log.Fatal(msg);
+        throw new ArgumentException(msg);
+      }
+      _trello.Authorize(this._authToken);
+      _log.InfoFormat("Authorized with token {0}",this._authToken);
+    }
+
+    private int timeoutRequests = 0;
+    private int totalRequests = 1;
+    private DateTime LastReset = DateTime.UtcNow;
+    private void addRequest()
+    {
+      timeoutRequests++;
+      totalRequests++;
+      if (timeoutRequests > 250)
+      {
+        _log.Info("Hit 250 requests.");
+        if ((DateTime.UtcNow - LastReset).TotalSeconds < 15)
+        {
+          _log.Info("nearing api limit, sleeping for a bit.");
+          System.Threading.Thread.Sleep(1000 * 15);
+        }
+        timeoutRequests = 0;
+        LastReset = DateTime.UtcNow;
+      }
+      else
+        System.Threading.Thread.Sleep(10); 
+      //completely arbitrary, just being a good citizen here.
+      //10 means we can hit up to 666 requests in the 10 second timespan.
+
+    }
+    public void Execute(IJobExecutionContext context)
+    {
+      var listIDs = System.Configuration.ConfigurationManager.AppSettings["ListIDs"];
+      if(listIDs == null) {
+        var myBoards = _trello.Boards.ForMe();
+        string listInfoStr = "";
+        if (myBoards != null)
+        {
+          var listInfo = myBoards.SelectMany(x => _trello.Lists.ForBoard(new BoardId(x.Id)).Select(l => l.Name + "-" + l.Id + "-board:" + x.Name));
+          listInfoStr = String.Join(Environment.NewLine, listInfo);
+        }
+        else
+          listInfoStr = "null response from trello, probably a bug in TrelloNet";
+        string msg = "Must have a ListIDs app settings with a semicolon separated list of list Ids to archive against. Your current list ids are: " + listInfoStr;
+        _log.Fatal(msg);
+        throw new ConfigurationException(msg);
+      }
+      var lists = listIDs.Split(';').Select(x =>
+      {
+        return new ArchiveSetting() { ListID = x };
+      });
+      int requests = 1; //1 because auth request.
+      int totalRequests = 0;
+      //rate limit is 300 / 10 seconds.
+      foreach (var list in lists)
+      {
+        _log.Trace("Examining list " + list.ListID);
+        var lid = list.ListID;
+        var theCards = _trello.Cards.ForList(new ListId(list.ListID),CardFilter.Open);
+        requests++;
+        foreach (var card in theCards)
+        {
+          if (card.DateLastActivity < DateTime.UtcNow.Subtract(list.Aging))
+          {
+            _trello.Cards.Archive(new CardId(card.Id));
+            _log.WarnFormat("Archived card {0} with name {1} from list {2} and board {3}", card.Id, card.Name, card.IdList, card.IdBoard);
+          }
+          else
+          {
+            _log.TraceFormat("Card {0} ({1}) has {2} days left.",card.Id,card.Name,(list.Aging - (DateTime.UtcNow - card.DateLastActivity)).TotalDays);
+          }
+        }
+      }
+      _log.Trace("Finished run. requests: " + totalRequests);
+      totalRequests = 0;
+      requests = 0;
+      //get all the trello data, then match it up with ours!
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Topshelf;
+using Quartz;
+using Quartz.Impl;
+using Common.Logging;
+using Common.Logging.Configuration;
+
+namespace TrelloArchiver
+{
+  public class Scheduler
+  {
+    private ILog _log = LogManager.GetCurrentClassLogger();
+    private IScheduler _scheduler;
+    public Scheduler()
+    {
+      // Grab the Scheduler instance from the Factory 
+      this._scheduler = StdSchedulerFactory.GetDefaultScheduler();
+    }
+    public Scheduler(IScheduler Scheduler)
+    {
+      this._scheduler = Scheduler;
+    }
+
+    public void Start()
+    {
+      try
+      {
+        // and start it off
+        _scheduler.Start();
+        // define the job and tie it to our HelloJob class
+        IJobDetail job = JobBuilder.Create<ArchiverJob>()
+            .WithIdentity("job1", "group1")
+            .Build();
+        // Trigger the job to run now, and then repeat every 10 seconds
+        ITrigger trigger = TriggerBuilder.Create()
+            .WithIdentity("trigger1", "group1")
+            .WithSimpleSchedule(x => x
+                .WithIntervalInHours(1)
+                .RepeatForever())
+            .Build();
+        // Tell quartz to schedule the job using our trigger
+        _scheduler.ScheduleJob(job, trigger);
+      }
+      catch (Exception ex)
+      {
+        _log.Fatal("Uncaught exception during scheduler start.", ex);
+        throw;
+      }
+    }
+    public void Stop()
+    {
+      // and last shut down the scheduler when you are ready to close your program
+      _scheduler.Shutdown();
+    }
+  }
+  class Program
+  {
+    private static ILog _log;
+    static void Main(string[] args)
+    {
+      NameValueCollection properties = new NameValueCollection();
+      properties["showDateTime"] = "true";
+      properties["level"] = "All";
+      properties["showLogName"] = "true";
+#if DEBUG
+      Common.Logging.LogManager.Adapter = new Common.Logging.Simple.ConsoleOutLoggerFactoryAdapter(properties);
+#endif
+      _log = LogManager.GetCurrentClassLogger();
+
+      try
+      {
+        HostFactory.Run(x => {
+            x.Service<Scheduler>(s =>                        //2
+            {
+              s.ConstructUsing(name => new Scheduler());     //3
+              s.WhenStarted(tc => tc.Start());              //4
+              s.WhenStopped(tc => tc.Stop());               //5
+            });
+            x.RunAsLocalSystem();                            //6
+            x.SetDescription("Trello Archiver - regularly archives cards.");        //7
+            x.SetDisplayName("TrelloArchiver");                       //8
+            x.SetServiceName("TrelloArchiver");                       //9
+            x.StartAutomaticallyDelayed();
+          });                                                  //10
+      }
+      catch (Exception ex)
+      {
+        _log.Fatal("Uncaught exception during program start.", ex);
+        throw;
+      }
+    }
+  }
+}
